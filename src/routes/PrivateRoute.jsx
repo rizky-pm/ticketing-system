96a202adde226d1de770720ed1d0331ab615ad8c
Rizky Putra Mahendra <rizkymahendra2346@gmail.com>
2022-09-22 10:31:06 +0700

    Added jwt-decode package

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import jwt_decode from 'jwt-decode';
 import { Navigate } from 'react-router-dom';
 
 import Navbar from '../features/Navbar/Navbar';
 
 const PrivateRoute = ({ children }) => {
   const user = JSON.parse(localStorage.getItem('user'));
+  // const tokenExp = jwt_decode(user.data.token).exp * 1000;
 
   if (!user) {
     return <Navigate to='login' replace />;
