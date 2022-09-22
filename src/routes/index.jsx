a3660b91b7624e7271ccc65dcefa3c04d9690d14
Rizky Putra Mahendra <rizkymahendra2346@gmail.com>
2022-09-22 11:08:33 +0700

    Updated router file to use createRoutesFromElements

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,4 +1,8 @@
-import { createBrowserRouter } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom';
 
 import PrivateRoute from './PrivateRoute';
 import HomePage from '../pages/HomePage/HomePage';
@@ -6,19 +10,21 @@ import LoginPage from '../pages/LoginPage/LoginPage';
 import '../styles.css';
 import '../styles.less';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: (
-      <PrivateRoute>
-        <HomePage />
-      </PrivateRoute>
-    ),
-  },
-  {
-    path: '/login',
-    element: <LoginPage />,
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route
+        path='/'
+        element={
+          <PrivateRoute>
+            <HomePage />
+          </PrivateRoute>
+        }
+      />
+
+      <Route path='/login' element={<LoginPage />} />
+    </>
+  )
+);
 
 export default router;
