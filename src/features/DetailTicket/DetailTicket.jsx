47f456d2031bbaa72ac9b1f3cba73e975f0bd67d
Rizky Putra Mahendra <rizkymahendra2346@gmail.com>
2022-09-23 16:06:15 +0700

    Improved detail ticket section

--- src/features/DetailTicket/DetailTicket.jsx
+++ src/features/DetailTicket/DetailTicket.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { LeftOutlined } from '@ant-design/icons';
+import { LeftOutlined, FileOutlined } from '@ant-design/icons';
+import moment from 'moment';
+import 'moment/locale/id';
 
 import { getTicketById } from '../../api';
 import './DetailTicket.scss';
+import { downloadFile, kebabCase } from '../../utils';
 
 const DetailTicket = () => {
   const [isFetching, setIsFetching] = useState(false);
@@ -40,6 +43,38 @@ const DetailTicket = () => {
               className='container--header__back-icon'
             />
             <h1 className='container--header__title'>Detail Ticket</h1>
+          </div>
+
+          <div className='container--content'>
+            <h1 className='container--content__title'>{ticket?.title}</h1>
+            <p className='container--content__customer-name'>
+              By {ticket?.customerName}
+            </p>
+            <p className='container--content__date'>
+              {moment(ticket?.createdAt).format('dddd, Do MMMM YYYY -- HH:mm')}
+            </p>
+            <p className='container--content__desc'>
+              {ticket?.description} Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Beatae accusamus ut iure perspiciatis illo esse
+              provident qui incidunt unde officiis officia ipsa, quaerat
+              inventore ratione aliquam eligendi. Accusamus, tenetur maxime.
+            </p>
+
+            {ticket?.fileId ? (
+              <div
+                className='container--content__attachment'
+                onClick={() => {
+                  downloadFile(ticket);
+                }}
+              >
+                <FileOutlined className='container--content__attachment--icon' />
+                <span className='container--content__attachment--fileName'>
+                  {kebabCase(ticket?.title) + '_attachment.' + ticket.fileExt}
+                </span>
+              </div>
+            ) : (
+              <p>There is no attachment</p>
+            )}
           </div>
         </>
       )}
--- src/utils/index.js
+++ src/utils/index.js
@@ -4,3 +4,28 @@ export const slugFromName = (name) => {
     .replace(/ /g, '-')
     .replace(/[^\w-]+/g, '');
 };
+
+export const kebabCase = (str) => {
+  return str
+    .replace(/([a-z])([A-Z])/g, '$1-$2')
+    .replace(/\s+/g, '-')
+    .toLowerCase();
+};
+
+export const downloadFile = (file) => {
+  let ext;
+
+  if (['bmp', 'jpg', 'jpeg', 'gif', 'png'].includes(file.fileExt)) {
+    ext = 'image/' + file.fileExt;
+  } else {
+    ext = 'application/' + file.fileExt;
+  }
+
+  const linkSource = `data:${ext};base64,${file.fileName}`;
+  const downloadLink = document.createElement('a');
+  let fileName = kebabCase(file.title) + '_attachment.' + file.fileExt;
+
+  downloadLink.href = linkSource;
+  downloadLink.download = fileName;
+  downloadLink.click();
+};
